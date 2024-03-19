764d642c9edef239c70dce60e21ad2fd83739705
albertoapedra-local <albertoapedra@albertoapedra.com>
2024-03-19 19:35:17 +0100

    NextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/libretema",
+    output: "export",
+    reactStrictMode: true,
+  };
 
 export default nextConfig;
